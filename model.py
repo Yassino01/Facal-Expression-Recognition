99ec69bcca8a06b758f1460d530dac336a23e6fb
Yassine Bareche <fy_bareche@esi.dz>
2021-05-12 02:17:33 +0200

    affine_grid upfated

--- model.py
+++ model.py
@@ -42,8 +42,8 @@ class Deep_Emotion(nn.Module):
     theta = self.fc_loc(xs)
     theta = theta.view(-1, 2, 3)
 
-    grid = F.affine_grid(theta, x.size())
-    x = F.grid_sample(x, grid)
+    grid = F.affine_grid(theta, x.size(), align_corners = True)
+    x = F.grid_sample(x, grid, align_corners = True)
     return x
 
   def forward(self,input):
